ec8ac70010f1022407a46944b6eefa5f34f5448f
David Weiner <davidweiner@dreamfactory.com>
2015-09-17 14:51:00 -0400

    Updated local IP address of target host, added dedicated setup and teardown scripts

--- readtest.js
+++ readtest.js
@@ -2,41 +2,6 @@ var uri = 'http://10.0.1.236:80/api/v2/DBTest';
 
 var flow = {
     before: [
-        {
-            // Create the test table
-
-            post: uri + '/_schema',
-            headers: {Accept: 'application/json, text/javascript, */*; q=0.01'},
-            json: {
-                resource: [
-                    {
-                        name: "test_table",
-                        field: [
-                            {
-                                name: "id",
-                                type: "id",
-                                is_primary_key: true,
-                                auto_increment: true
-                            },
-                            {
-                                name: "name",
-                                type: "string"
-                            }
-                        ]
-                    }
-                ]
-            }
-        },
-        {
-            // Insert a record
-
-            post: uri + '/_table/test_table',
-            headers: {Accept: 'application/json, text/javascript, */*; q=0.01'},
-            json: {
-                id: null,
-                name: "READ TEST"
-            }
-        }
     ],
     beforeMain: [],
     main: [
@@ -49,12 +14,6 @@ var flow = {
     ],
     afterMain: [],
     after: [
-        {
-            // Delete the table
-
-            del: uri + '/_schema/test_table',
-            headers: {Accept: 'application/json, text/javascript, */*; q=0.01'}
-        }
     ]
 };
 
