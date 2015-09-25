0dabc53de516de2f1590d900655a69f050d5211b
David Weiner <davidweiner@dreamfactory.com>
2015-09-25 09:49:32 -0400

    Updated setup so that multiple records can be created if desired

--- setup.js
+++ setup.js
@@ -1,35 +1,33 @@
 var uri = 'http://10.0.1.236:80/api/v2/DBTest';
 
 var flow = {
-    before: [
-    ],
-    beforeMain: [],
-    main: [
-        {
+    before: [],
+    beforeMain: [{
         // Create the test table
 
-            post: uri + '/_schema',
-            headers: {Accept: 'application/json, text/javascript, */*; q=0.01'},
-            json: {
-                resource: [
-                    {
-                        name: "test_table",
-                        field: [
-                            {
-                                name: "id",
-                                type: "id",
-                                is_primary_key: true,
-                                auto_increment: true
-                            },
-                            {
-                                name: "name",
-                                type: "string"
-                            }
-                        ]
-                    }
-                ]
-            }
-        },
+        post: uri + '/_schema',
+        headers: {Accept: 'application/json, text/javascript, */*; q=0.01'},
+        json: {
+            resource: [
+                {
+                    name: "test_table",
+                    field: [
+                        {
+                            name: "id",
+                            type: "id",
+                            is_primary_key: true,
+                            auto_increment: true
+                        },
+                        {
+                            name: "name",
+                            type: "string"
+                        }
+                    ]
+                }
+            ]
+        }
+    }],
+    main: [
         {
             // Insert a record
 
@@ -42,8 +40,7 @@ var flow = {
         }
     ],
     afterMain: [],
-    after: [
-    ]
+    after: []
 };
 
 module.exports = flow;
